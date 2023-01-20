269b8a1c0aed2ef90de6be263caa7f9606661934
AvFl4028 <aviel4028@gmail.com>
2023-01-20 12:23:29 -0600

    update

--- cpu-arduino.ino
+++ cpu-arduino.ino
@@ -17,51 +17,138 @@ Servo motor;
 // Pin 6
 
 // DATA
-char serialData;
-String info;
+  // CHAR
+    char serialData;
+  // STRING
+    String info;
+    String modes[] = {"Commands", "Motor", "Info"};
+    String commandsTxt[] = {"ls", "mkdir", "neofetch"};
+
+  // INT
+    int modeData = 0;                           // Verifica si esta conectado el bluetooth
+    int y;                                  // Parte de la funcion de selector de menu
+    int x;
+    int len = sizeof(modes) / sizeof(modes[0]); // Tamaño del array de modes[]
+    int commandLen = sizeof(commandsTxt) / sizeof(commandsTxt[0]);
 
 void setup() {
   bluetooth.begin(9600);
   bluetooth.println("Connected");
+  Serial.begin(9600);
+
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print("Initializing...");
   delay(1000);
   lcd.clear();
+
   motor.attach(6);
 }
 
 void loop() {
   if (bluetooth.available())
   {
-    lcd.clear();
+    modeData = 1;
     serialData = bluetooth.read();
-    info += serialData;
-    mode();
+    clean(serialData);
   }
+  mode();
+}
+
+//NOTE - Funcion de modo
+void mode(){
+  if (modeData == 1)
+  {
+    menu();
+  }else if(modeData == 0){
     lcd.setCursor(0, 0);
     lcd.print("Esperando");
     lcd.setCursor(0, 1);
     lcd.print("Conexion");
-    delay(200);
+  }
+  Serial.println(modeData);
 }
 
 //NOTE - Funcion de menu
-void mode(){
-    lcd.clear();
-    info = "";
+void menu(){
+  lcd.clear();
+
+  select();
+  selectMenu();
+  subMenu();
+  /*+
     lcd.setCursor(0,0);
-    lcd.print("Select mode:");
-    lcd.setCursor(0,1);
-    lcd.print("1)Commands   2)Motor");
-    if (info == "Command")
+    lcd.print(serialData);
+
+    lcd.setCursor(0, 1);
+    lcd.print(len);
+*/
+  delay(200);
+}
+
+void select(){
+  switch (serialData)
+  {
+  case '1':
+    y--;
+    serialData = '0';
+    break;
+  case '2':
+    y++;
+    serialData = '0';
+    break;
+  case '3':
+    x = 1;
+    serialData = '0';
+    break;
+  case '4':
+    x = 0;
+    serialData = '0';
+    break;
+  default:
+    break;
+  }
+  if (y > len - 1 || y < 0)
+  {
+    y = 0;
+  }
+}
+
+void selectMenu()
+{
+  if (y == 0)
+  {
+    lcd.setCursor(0,0);
+    lcd.print(">" + modes[0]);
+    lcd.setCursor(1, 1);
+    lcd.print(modes[1]);
+  }
+  if (y == 1)
+  {
+    lcd.setCursor(1, 0);
+    lcd.print(modes[0]);
+    lcd.setCursor(0, 1);
+    lcd.print(">" + modes[1]);
+  }
+  if (y == 2)
+  {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(">" + modes[2]);
+  }
+}
+
+void subMenu(){
+  if (x == 1)
+  {
+    lcd.clear();
+    if (y == 0)
     {
-      lcd.clear();
-      lcd.print("--Command Mode--");
-      delay(1000);
-      lcd.clear();
-      }
+      commands();
+    }
+  }
+  return;
 }
 
 //NOTE - Funcion de limpiar pantalla
@@ -80,9 +167,17 @@ void clean(char data)
 }
 
 //NOTE - Funcion de comandos
-void commands(String string){
-  
+//TODO - Cambiar los comandos a una lista
+
+void commands(){
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("--Command Mode--");
+  delay(500);
+  lcd.clear();
+
   String file;
+  String string;
   
   if (string == "ls")
   {
@@ -115,7 +210,6 @@ void commands(String string){
     lcd.print("SO: Test");  
   
   }else{
-    
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Command error");
@@ -124,6 +218,7 @@ void commands(String string){
 }
 
 //NOTE - Funcion de controlar servomotor
+//REVIEW - NO SE HA PROBADO
 void Motor(char i){
   int angle = map(i, 0, 9, 0, 180);
   if (i < 180)
